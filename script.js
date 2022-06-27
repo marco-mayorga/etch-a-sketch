858b80c5b52247b81f716723089da8b4d31d2f94
Marco Mayorga <marcomayorga@Marcos-MacBook-Air.local>
2022-06-27 06:13:14 -0500

    add sliderText that changes with slider value

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+// Selects the slider and makes the div above display grid size
+let sliderValue = document.querySelector(".slider");
+let sliderValueDiv = document.querySelector(".sliderText");
+sliderValueDiv.innerHTML = `${sliderValue.value}X${sliderValue.value}`;
+sliderValue.oninput = function() {
+    sliderValueDiv.innerHTML = `${this.value}X${this.value}`;
+}
+
 // Creates a 16 by 16 grid adds the gridItem class and appends it to gridContainer
 for(let i = 1; i <= 16*16; i++){
     let gridItem = document.createElement("div");
     gridItem.className = "gridItem";
     document.getElementById("gridContainer").appendChild(gridItem);
 };
-
-
