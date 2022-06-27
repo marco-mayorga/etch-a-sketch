d6a08fd2f544bcd2c05126438f321e109edebca7
Marco Mayorga <marcomayorga@Marcos-MacBook-Air.local>
2022-06-27 02:22:26 -0500

    remove text from divs in grid

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-for(let i = 1; i < 16*16; i++){
+// Creates a 16 by 16 grid adds the gridItem class and appends it to gridContainer
+for(let i = 1; i <= 16*16; i++){
     let gridItem = document.createElement("div");
     gridItem.className = "gridItem";
     document.getElementById("gridContainer").appendChild(gridItem);
-    gridItem.textContent = i;
 };
+
+
