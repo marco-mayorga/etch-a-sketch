c232676ac28c36f0a1e1c26bb8290e18265d7125
Marco Mayorga <marcomayorga@Marcos-MacBook-Air.local>
2022-07-03 04:14:14 -0500

    restructure code to make it more lightweight and add slider functionality

--- script.js
+++ script.js
@@ -1,89 +1,56 @@
-// Selects the slider and makes the div above display grid size
+let color = "white"
+
+// Selects the slider
 let sliderValue = document.querySelector(".slider");
 let sliderValueDiv = document.querySelector(".sliderText");
-sliderValueDiv.innerHTML = `${sliderValue.value}X${sliderValue.value}`;
-let gridArea = sliderValue.value**2;
+
+// Changes div above slider depending on value
 sliderValue.oninput = function() {
     sliderValueDiv.innerHTML = `${this.value}X${this.value}`;
 }
 
+function makeBoard(size){
+    let gridContainer = document.querySelector(".gridContainer");
+    let gridItems = gridContainer.querySelectorAll("div");
+    gridItems.forEach((div) => (div.remove()));
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-// Creates a 16 by 16 grid adds the gridItem class and appends it to gridContainer
-for(let i = 1; i <= gridArea; i++){
-    let gridItems = document.createElement("div");
-    let gridContainer = document.getElementById("gridContainer");
-    gridItems.className = "gridItem";
-    gridContainer.appendChild(gridItems);
-}
+    // Creates a default 16 by 16 grid adds the gridItem class and appends it to gridContainer
+    let amount = size*size
+    for(let i = 0; i <= amount; i++){
+        let gridItems = document.createElement("div");
+        gridItems.style.background = "black"
+        gridContainer.insertAdjacentElement("beforeend",gridItems);
+        gridItems.addEventListener("mouseover", colorOfGrid);
+}}
 
+makeBoard(16);
 
+function chooseSize(input){
+    makeBoard(input);
+} 
 
-const buttons = document.querySelectorAll("button");
-const colorMode = buttons[0];
-const randomColorMode = buttons[1];
-const eraser  = buttons[2];
-const clearSketch = buttons[3];
-let gridItems = document.querySelectorAll(".gridItem");
-for (const gridItem of gridItems){
-    gridItem.addEventListener("mouseover", colorGridItems);
-}
-colorMode.addEventListener("click", () => {
-    colorMode.classList.toggle("clickedButton");
-    randomColorMode.classList = "button";
-    eraser.classList = "button";
-    for (const gridItem of gridItems){
-        gridItem.removeEventListener("mouseover", randomColorModeFunc);
-        gridItem.removeEventListener("mouseover", eraserFunc);
-        gridItem.addEventListener("mouseover", colorGridItems);
-    }
-})
-randomColorMode.addEventListener("click", () => {
-    randomColorMode.classList.toggle("clickedButton");
-    colorMode.classList = "button";
-    eraser.classList = "button";
-    for (gridItem of gridItems){
-        gridItem.removeEventListener("mouseover", colorGridItems);
-        gridItem.removeEventListener("mouseover", eraserFunc);
-        gridItem.addEventListener("mouseover", randomColorModeFunc);
-    }
-})
-eraser.addEventListener("click", () => {
-    eraser.classList.toggle("clickedButton");
-    colorMode.classList = "button";
-    randomColorMode.classList = "button";
-    for (const gridItem of gridItems){
-        gridItem.removeEventListener("mouseover", colorGridItems);
-        gridItem.removeEventListener("mouseover", randomColorModeFunc);
-        gridItem.addEventListener("mouseover", eraserFunc);
-    }
-    
-})
-
-clearSketch.addEventListener("click", () => {
-    for (const gridItem of gridItems){
-        gridItem.style.backgroundColor = "#000000";
-    }
-})
-
-// Change etch a sketch color
-function colorGridItems() {
-    let colorPickerValue = document.querySelector("#colorPicker");
-    this.style.backgroundColor = `${colorPickerValue.value}`;
-}
-
-// etch a sketch eraser
-
-function eraserFunc(){
-    for (const gridItem of gridItems){
-        this.style.backgroundColor = "black"
+function colorOfGrid(){
+    if (color === 'random'){
+        let randomR = Math.floor(Math.random() * 256);
+        let randomG = Math.floor(Math.random() * 256);
+        let randomB = Math.floor(Math.random() * 256);
+        this.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    }else if (color === 'customColor') {
+        let colorPickerValue = document.querySelector("#colorPicker");
+        this.style.backgroundColor = `${colorPickerValue.value}`;
+    }else{
+        this.style.backgroundColor = color;
     }
 }
 
-// random etch a sketch color
-function randomColorModeFunc() {
-    let randomR = Math.floor(Math.random() * 256);
-    let randomG = Math.floor(Math.random() * 256);
-    let randomB = Math.floor(Math.random() * 256);
-    this.style.backgroundColor =`rgb(${randomR}, ${randomG}, ${randomB})`;
-    console.log(gridItems.style.backgroundColor);
+function chooseColor(userChoice){
+    color = userChoice;
+}
+
+function resetSketchBoard(){
+    let gridContainer = document.querySelector(".gridContainer");
+    let gridItems = gridContainer.querySelectorAll("div");
+    gridItems.forEach((div) => div.style.backgroundColor = "black");
 }
